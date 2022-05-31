a649f49c8b77272a26681c45e187f9c618ed35ea
Jackson Merma <jmermap@unsa.edu.pe>
2022-05-30 21:17:36 -0500

    implementacion de opcion para generar un crontab (aun no realiza cambios)

--- scripts/pandor/prueba.sh
+++ scripts/pandor/prueba.sh
@@ -8,9 +8,30 @@
 ######################################################################
 
 #gnome-terminal --geometry 60x20 -e "whiptail --msgbox --title 'titulo' 'Que onda chaval' 17 53"
-NOMBRE=$(whiptail --inputbox 'Cual es tu nombre?' 17 53 --title 'Pregunta' 3>&1 1>&2 2>&3)
+#COMANDO=$(whiptail --inputbox 'Comando' 17 53 --title '<<Pandor>>' 3>&1 1>&2 2>&3)
 
-whiptail --msgbox --title "titulo" "Hola, $NOMBRE, como estais?" 17 53
+
+#if [[ $COMANDO != "" ]]
+#then
+#	whiptail --msgbox --title "titulo" " comando es >> $COMANDO" 17 53
+#fi
+
+COMANDO=""
+
+while [[ $COMANDO == "" ]]
+do
+	COMANDO=$(whiptail --inputbox 'Comando' 17 53 --title '<<Pandor>>' 3>&1 1>&2 2>&3)
+done
+
+
+INPUT_TIME=""
+INPUT_COMMAND=""
+
+if [[ $COMANDO == "crontab" ]]
+then
+	INPUT_TIME=$(whiptail --inputbox 'MIN HOR DOM MON DOW' 17 53 --title '<<Pandor|Crontab>>' 3>&1 1>&2 2>&3)
+	INPUT_COMMAND=$(whiptail --inputbox 'Ingresa comando' 17 53 --title '<<Pandor|Crontab>>' 3>&1 1>&2 2>&3)
+fi
 
 
 
