c9ba02cf73c0e43d6324b5b131fcfb58ae0bad3a
Jackson Merma <jmermap@unsa.edu.pe>
2022-05-30 19:27:50 -0500

    script simple para abrir una terminal y ejecutar whiptail

---
+++ scripts/pandor/prueba.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+######################################################################
+# @author      : Jackson Merma (mermaportocarreroj@gmail.com)
+# @file        : prueba
+# @created     : 30/05/2022
+# @description : 
+######################################################################
+
+gnome-terminal --geometry 60x20 -e "whiptail --msgbox --title 'titulo' 'Que onda chaval' 10 30"
+
+
