33cdf60b9888a42ddd8886d16063136831f22e0a
Jackson Merma <jmermap@unsa.edu.pe>
2022-05-30 19:51:58 -0500

    cambio de metodologia para abrir la terminal e input implementado

--- scripts/pandor/prueba.sh
+++ scripts/pandor/prueba.sh
@@ -7,6 +7,10 @@
 # @description : 
 ######################################################################
 
-gnome-terminal --geometry 60x20 -e "whiptail --msgbox --title 'titulo' 'Que onda chaval' 10 30"
+#gnome-terminal --geometry 60x20 -e "whiptail --msgbox --title 'titulo' 'Que onda chaval' 17 53"
+NOMBRE=$(whiptail --inputbox 'Cual es tu nombre?' 17 53 --title 'Pregunta' 3>&1 1>&2 2>&3)
+
+whiptail --msgbox --title "titulo" "Hola, $NOMBRE, como estais?" 17 53
+
 
 
