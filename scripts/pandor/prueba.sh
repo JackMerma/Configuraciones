b3d4df919a3d9e3d59a30d1987c7c577d2accb7c
Jackson Merma <jmermap@unsa.edu.pe>
2022-05-30 23:01:14 -0500

    crontab modulado (opcion de gui command)

--- scripts/pandor/prueba.sh
+++ scripts/pandor/prueba.sh
@@ -29,9 +29,24 @@ INPUT_COMMAND=""
 
 if [[ $COMANDO == "crontab" ]]
 then
+	#path del archivo principal para crontabs
+	CRON_PATH=/home/jackson/Configuraciones/scripts/pandor/cronfile.txt
+	GUI_OPTION=""
+
+	#entrada de configuracion del tiempo
 	INPUT_TIME=$(whiptail --inputbox 'MIN HOR DOM MON DOW' 17 53 --title '<<Pandor|Crontab>>' 3>&1 1>&2 2>&3)
+
+	#si usará un GUI command
+	if (whiptail --yesno '¿Comando gráfico?' 17 53 --yes-button 'Si' --title '<<Pandor|Crontab|Gui>>')
+	then
+		GUI_OPTION="export DISPLAY=:0 &&"
+	fi
+
+	#entrada de comando
 	INPUT_COMMAND=$(whiptail --inputbox 'Ingresa comando' 17 53 --title '<<Pandor|Crontab>>' 3>&1 1>&2 2>&3)
+
+	#escritura y nueva lectura en crontab
+	echo "$INPUT_TIME $GUI_OPTION $INPUT_COMMAND" >> $CRON_PATH
+	crontab -u jackson $CRON_PATH
+	whiptail --msgbox "$INPUT_COMMAND guardado." 17 53 --title '<<Pandor|Crontab|Salida>>'
 fi
-
-
-
