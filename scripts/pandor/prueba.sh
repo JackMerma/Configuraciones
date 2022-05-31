3c5968377c4021804e9202fb6ebc2aa3aafead88
Jackson Merma <jmermap@unsa.edu.pe>
2022-05-30 23:12:31 -0500

    estandarizacion de variables x/y

--- scripts/pandor/prueba.sh
+++ scripts/pandor/prueba.sh
@@ -7,20 +7,22 @@
 # @description : 
 ######################################################################
 
-#gnome-terminal --geometry 60x20 -e "whiptail --msgbox --title 'titulo' 'Que onda chaval' 17 53"
-#COMANDO=$(whiptail --inputbox 'Comando' 17 53 --title '<<Pandor>>' 3>&1 1>&2 2>&3)
+#gnome-terminal --geometry 60x20 -e "whiptail --msgbox --title 'titulo' 'Que onda chaval' $DIMX $DIMY"
+#COMANDO=$(whiptail --inputbox 'Comando' $DIMX $DIMY --title '<<Pandor>>' 3>&1 1>&2 2>&3)
 
 
 #if [[ $COMANDO != "" ]]
 #then
-#	whiptail --msgbox --title "titulo" " comando es >> $COMANDO" 17 53
+#	whiptail --msgbox --title "titulo" " comando es >> $COMANDO" $DIMX $DIMY
 #fi
 
 COMANDO=""
+DIMX=9
+DIMY=53
 
 while [[ $COMANDO == "" ]]
 do
-	COMANDO=$(whiptail --inputbox 'Comando' 17 53 --title '<<Pandor>>' 3>&1 1>&2 2>&3)
+	COMANDO=$(whiptail --inputbox 'Comando' $DIMX $DIMY --title '<<Pandor>>' 3>&1 1>&2 2>&3)
 done
 
 
@@ -34,19 +36,19 @@ then
 	GUI_OPTION=""
 
 	#entrada de configuracion del tiempo
-	INPUT_TIME=$(whiptail --inputbox 'MIN HOR DOM MON DOW' 17 53 --title '<<Pandor|Crontab>>' 3>&1 1>&2 2>&3)
+	INPUT_TIME=$(whiptail --inputbox 'MIN HOR DOM MON DOW' $DIMX $DIMY --title '<<Pandor|Crontab>>' 3>&1 1>&2 2>&3)
 
 	#si usará un GUI command
-	if (whiptail --yesno '¿Comando gráfico?' 17 53 --yes-button 'Si' --title '<<Pandor|Crontab|Gui>>')
+	if (whiptail --yesno '¿Comando gráfico?' $DIMX $DIMY --yes-button 'Si' --title '<<Pandor|Crontab|Gui>>')
 	then
 		GUI_OPTION="export DISPLAY=:0 &&"
 	fi
 
 	#entrada de comando
-	INPUT_COMMAND=$(whiptail --inputbox 'Ingresa comando' 17 53 --title '<<Pandor|Crontab>>' 3>&1 1>&2 2>&3)
+	INPUT_COMMAND=$(whiptail --inputbox 'Ingresa comando' $DIMX $DIMY --title '<<Pandor|Crontab>>' 3>&1 1>&2 2>&3)
 
 	#escritura y nueva lectura en crontab
 	echo "$INPUT_TIME $GUI_OPTION $INPUT_COMMAND" >> $CRON_PATH
 	crontab -u jackson $CRON_PATH
-	whiptail --msgbox "$INPUT_COMMAND guardado." 17 53 --title '<<Pandor|Crontab|Salida>>'
+	whiptail --msgbox "$INPUT_COMMAND guardado." $DIMX $DIMY --title '<<Pandor|Crontab|Salida>>'
 fi
